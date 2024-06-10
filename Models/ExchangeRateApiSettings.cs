c7ad3f0a9039fb76bc960f4fde91d5772fdf51d7
Mohammad Mhsoun <m.mhsoon@gmail.com>
2024-06-10 23:16:06 +0200

    .gitignore is now working

--- Models/ExchangeRateApiSettings.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace CurrencyDeltaGrpc.Models;
-public class ExchangeRateApiSettings
-{
-    public string BaseUrl { get; set; } = string.Empty;
-    public string ApiKey { get; set; } = string.Empty;
-}
